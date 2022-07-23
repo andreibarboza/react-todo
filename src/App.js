65ae5322a61ae610455263eefd0f865fe05e17f8
andreibarboza <andreibarboza97@gmail.com>
2022-07-23 12:56:33 -0300

    refactor: removing useless component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import { NoteList } from './components/NoteList';
 import { useEffect, useState } from 'react';
 import { Styles } from './AppStyles.jsx';
+import { NoteExample } from './components/NoteExample'
 
 export const App = () => {
   const [child, setChild] = useState([]);
@@ -28,13 +28,15 @@ export const App = () => {
           <span>Welcome to your To Do list!</span>
           <div>
             <input type="text" value={text} onChange={(e) => setText(e.target.value)} />
-            <button type="button" className="button-create" onClick={handleAdd}>
+            <button type="submit" className="button-create" onClick={handleAdd}>
                   Create new To Do
             </button>
           </div>
         </header>
         <Styles.Notes>
-          <NoteList noteList={child} deleteNote={deleteNote}/>
+          {child.map((text, index) => (
+            <NoteExample key={index} text={text} deleteNote={() => deleteNote(index)}/>
+          ))}
         </Styles.Notes>
       </Styles.Center>
     </Styles.Content>
